22519b50624f03736d7a06352c65268d15cb8b31
Seth Baldridge <sethbaldridge87@gmail.com>
2019-05-06 14:32:25 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+          <div className="parallax-container">
+      <div className="parallax"><img src="https://www.adorama.com/alc/wp-content/uploads/2018/08/san-juans-feature-825x465.jpg" alt="none"/></div>
+    </div>
+    <div className="section white">
+      <div className="row container">
+        <h2 className="header">Parallax</h2>
+        <h1>Better stop</h1>
+        <h1>Better stop</h1>
+        <h1>Better stop</h1>
+        <h1>Better stop</h1>
+        <p className="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+      </div>
+    </div>
+    <div className="parallax-container">
+      <div className="parallax"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX7B4jQjqApi2e7uj0zj0KlQwPtru5DbqS8gpEf1Z4z9mZoOdb" alt="none"/></div>
+    </div>
+    
+    <h1>Better stop</h1>
+        <h1>Better stop</h1>
+        <h1>Better stop</h1>
+        <h1>Better stop</h1>
     </div>
   );
 }
